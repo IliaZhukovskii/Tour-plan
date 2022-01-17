52ed698813ef179f7085f0e6706e33f020d18f28
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-01-17 18:39:59 +0300

    create: add slider to hotel

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  }
+
+});
