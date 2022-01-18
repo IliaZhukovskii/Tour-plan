27513182c255bd64a926c3bc7348ef1c7d2817f9
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-01-18 22:50:08 +0300

    create: add booking an map

---
+++ js/map.js
@@ -0,0 +1,10 @@
+ymaps.ready(init);
+
+          function init() {
+               var myMap = new ymaps.Map("map", {
+                    center: [3.864781, 11.516019],
+                    zoom: 7
+               });
+          }
+          
+     
