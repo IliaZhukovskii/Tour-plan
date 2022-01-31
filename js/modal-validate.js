baabc0a2454a7b2c9e1d3f59c322780ed25be0e3
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-01-31 13:04:20 +0300

    create: made validate form

---
+++ js/modal-validate.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+  
+  $(".form-validate").each(function (){
+    $(this).validate({
+      messages: {
+        name:{
+          required:"Enter name",
+          minlength: "Name must be at least 2 letters",
+        },
+        phone: {
+          required: "Enter phone number",
+          minlength: "Phone must be at least 7 letters",
+        },
+        email: {
+          required: "Your email address must be in the format of name@domain.com",
+        }
+      }
+    })
+  })
+    
+  $('.number-mask').mask('+7(000) 000-00-00');
+  
+
+});
+
+
+
+
